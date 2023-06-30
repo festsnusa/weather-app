9a2f2a8b317abc71c24a3d7a76169b9ce3cc257a
festsnusa <lukatimof@mail.ru>
2023-06-30 19:02:36 +0700

    footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+footer.footer
+  .footer__left
+    span Made by Festsnusa 
+  .footer__right
+    span Contacts:
+    ul.contacts 
+      li.contact
+        a(href="https://t.me/onetrickponiee" target="_blank") 
+          img(src="@/assets/images/telegram.svg", alt="telegram")
+      li.contact
+        a(href="https://github.com/festsnusa" target="_blank") 
+          img(src="@/assets/images/github.svg", alt="github")
+      li.contact 
+        a(href="https://www.codewars.com/users/madhaus" target="_blank") 
+          img(src="@/assets/images/codewars.svg", alt="codewars")
+
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 2.5rem;
+
+  &__right {
+    display: flex;
+    flex-direction: column;
+
+    span {
+      margin: 0 auto;
+    }
+  }
+}
+
+ul {
+  list-style: none;
+}
+
+.contacts {
+  display: flex;
+}
+
+.contact {
+  img {
+    width: 2rem;
+    background-color: #fff;
+    margin: 1rem;
+    border-radius: 50%;
+  }
+}
+</style>
